5874b5d841a4eea08ed8843ce30929b8b310c594
yahuy-pendle <gia.huy@pendle.finance>
2025-04-16 17:43:22 +0700

    Fix error decoder (#55)
    * Fix error decoder
    
    * Fix

--- src/errors/helpers.ts
+++ src/errors/helpers.ts
@@ -1,6 +1,36 @@
-import { Hex, RawContractError, BaseError } from 'viem';
+import {
+  Hex,
+  RawContractError,
+  BaseError,
+  isHex,
+  AbiFunctionSignatureNotFoundError,
+  AbiEventSignatureNotFoundError,
+} from 'viem';
+
+const excludeErrorWithSignature = (err: Error): boolean => {
+  return err instanceof AbiEventSignatureNotFoundError || err instanceof AbiFunctionSignatureNotFoundError;
+};
+
+const extractErrorSignature = (err: Error | undefined): Hex | undefined => {
+  if (!err || excludeErrorWithSignature(err)) {
+    return undefined;
+  }
+  if ('signature' in err && isHex(err.signature)) {
+    return err.signature as Hex;
+  }
+  if (err.cause instanceof Error) {
+    return extractErrorSignature(err.cause);
+  }
+  return undefined;
+};
 
 export const extractErrorData = (err: Error): Hex | undefined => {
+  const signature = extractErrorSignature(err);
+
+  if (signature) {
+    return signature;
+  }
+
   const error = (
     err instanceof RawContractError
       ? err
