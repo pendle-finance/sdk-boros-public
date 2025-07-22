2919e7149fab7b3daf3861deb9e300d074f27433
cabonara <127368810+cabonara@users.noreply.github.com>
2025-07-22 11:11:13 +0200

    Add block number (#126)
    * add block number
    
    * v0.3.53

--- src/entities/publicClient.ts
+++ src/entities/publicClient.ts
@@ -1,8 +1,8 @@
-import { base } from 'viem/chains';
+import { arbitrum } from 'viem/chains';
 import { RPC_URL } from '../common';
 import { createPublicClient, http, PublicClient } from 'viem';
 
 export const publicClient = createPublicClient({
-  chain: base,
+  chain: arbitrum,
   transport: http(RPC_URL),
 }) as PublicClient;
