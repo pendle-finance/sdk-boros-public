f7a2afdb0072494a74b198dae6ce3ac60e053ba7
saobangmath <anhtai2672000@gmail.com>
2025-07-24 14:08:50 +0700

    v0.3.59: update claim rewards func for lp amm

--- src/entities/multiTokenMerkleDistributor/index.ts
+++ src/entities/multiTokenMerkleDistributor/index.ts
@@ -2,3 +2,4 @@ export * from './constants';
 export * from './referralRewardsDistributor';
 export * from './multiTokenMerkleDistributorContract';
 export * from './makerIncentiveRewardsDistributor';
+export * from './lpRewardsDistributor';
---
+++ src/entities/multiTokenMerkleDistributor/lpRewardsDistributor.ts
@@ -0,0 +1,28 @@
+import { Address } from 'viem';
+import { BorosBackend } from '../../backend';
+import { RewardDistributor } from './RewardDistributor';
+import { LP_REWARDS_MERKLE_DISTRIBUTOR_ADDRESS, MAKER_INCENTIVE_MERKLE_DISTRIBUTOR_ADDRESS } from './constants';
+
+export class LpRewardsDistributor extends RewardDistributor {
+  private borosCoreSdk: BorosBackend.BorosCoreSdk;
+  constructor() {
+    super(LP_REWARDS_MERKLE_DISTRIBUTOR_ADDRESS);
+    this.borosCoreSdk = BorosBackend.getCoreSdk();
+  }
+
+  async getMerkleByUser(user: Address): Promise<{
+    tokens: Address[];
+    accruedAmounts: bigint[];
+    proofs: string[][];
+  }> {
+    const resp = await this.borosCoreSdk.merkels.merklesControllerGetMerkleByUserAndCampaign('amm_lp_rewards', user);
+    const { tokens, accruedAmounts, proofs } = resp.data;
+    return {
+      tokens: tokens.map((token) => token as Address),
+      accruedAmounts: accruedAmounts.map((amount) => BigInt(amount)),
+      proofs,
+    };
+  }
+}
+
+export default LpRewardsDistributor;
