9a53451bdcd21371375be019502cc5e019843807
cabonara <lam@pendle.finance>
2025-08-01 17:22:48 +0700

    remove export

--- src/entities/multiTokenMerkleDistributor/index.ts
+++ src/entities/multiTokenMerkleDistributor/index.ts
@@ -1,4 +1,3 @@
-export * from './constants'; 
 export * from './referralRewardsDistributor';
 export * from './multiTokenMerkleDistributorContract';
 export * from './makerIncentiveRewardsDistributor';
